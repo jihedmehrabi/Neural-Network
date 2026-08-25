1090fe97d8daf631cf0fb2752639bc851cd4d713
jihedmehrabi <jihedbelhajyahia@gmail.com>
2026-08-25 18:00:44 +0100

    Privatize NeuralNetwork

--- 9-neural_network.py
+++ 9-neural_network.py
@@ -14,7 +14,7 @@ class NeuralNetwork:
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-        self.__W2 = np.random.randn(nodes, nx)
+        self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
