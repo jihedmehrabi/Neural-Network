b3da81e83f7e234e2060f49d682c4d642a295c7f
jihedmehrabi <jihedbelhajyahia@gmail.com>
2026-08-25 17:54:01 +0100

    neural_network_init_correction

--- 8-neural_network.py
+++ 8-neural_network.py
@@ -14,6 +14,6 @@ class NeuralNetwork:
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.randn(nodes, nx)
-        self.b2 = np.zeros((nodes, 1))
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
         self.A2 = 0
