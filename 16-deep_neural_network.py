2fcbb8c602c8820d06e4a8fe756ab6b75864a2e9
jihedmehrabi <jihedbelhajyahia@gmail.com>
2026-08-27 17:15:12 +0100

    deep_neural_network_init

---
+++ 16-deep_neural_network.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+
+
+class DeepNeuralNetwork:
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        if not isinstance(layers[0], int) or layers[0] <= 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.weights = {f"W{1}": np.random.randn(layers[0], nx) * math.sqrt(2 / nx),
+                        f"b{1}": np.zeros([1, layers[0]])}
+        for i in range(1, self.L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights[f"W{i + 1}"] = np.random.randn(layers[i], layers[i - 1]) * math.sqrt(2 / layers[i - 1])
+            self.weights[f"b{i + 1}"] = np.zeros([1, layers[i]])
